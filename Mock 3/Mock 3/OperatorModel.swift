8215b970a7818297b522846f07583b2414b9ca23
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-13 23:21:16 +0530

    Initial commit

---
+++ Mock 3/Mock 3/OperatorModel.swift
@@ -0,0 +1,15 @@
+//
+//  OperatorModel.swift
+//  Mock 3
+//
+//  Created by Akila Gamage on 2024-11-26.
+//
+
+import Foundation
+
+enum Operator:String, CaseIterable{
+    case add = "+"
+    case subtract = "-"
+    case multiply = "*"
+    case divide = "/"
+}
