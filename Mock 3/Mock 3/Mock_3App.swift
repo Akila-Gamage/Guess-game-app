5ab6ffd4bde1995fe8ef8871b556e188fcb3a692
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-13 23:21:13 +0530

    Initial commit

---
+++ Mock 3/Mock 3/Mock_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Mock_3App.swift
+//  Mock 3
+//
+//  Created by Akila Gamage on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct Mock_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
