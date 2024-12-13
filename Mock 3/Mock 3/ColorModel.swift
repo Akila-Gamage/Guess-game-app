74015892fd24d8c9e288f3405840785a2bdf038f
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-13 23:21:02 +0530

    Initial commit

---
+++ Mock 3/Mock 3/ColorModel.swift
@@ -0,0 +1,15 @@
+//
+//  ColorModel.swift
+//  Mock 3
+//
+//  Created by Akila Gamage on 2024-11-26.
+//
+
+import Foundation
+
+enum FontColor: String, CaseIterable {
+    case Sapphire = "Sapphire"
+    case Ruby = "Ruby"
+    case Emerald = "Emerald"
+}
+
